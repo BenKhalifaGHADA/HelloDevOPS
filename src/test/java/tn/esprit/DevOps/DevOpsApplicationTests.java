ce72b9efebb4aace309b8a81301c9657b3f72f10
Ben khalifa Ghada <benkhalifaghada@gmail.com>
2023-03-23 10:36:37 +0100

    First Commit

---
+++ src/test/java/tn/esprit/DevOps/DevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.DevOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
